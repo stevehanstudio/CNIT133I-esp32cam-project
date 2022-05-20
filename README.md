fdaeeec3320515f261073a4d0e683d40597cbc7b
Steve <15229911+stevehanstudio@users.noreply.github.com>
2022-05-20 03:17:51 -0700

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,43 @@
-# Getting Started with Create React App
+# Video From ESP32-CAM
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was created for the course [CNIT 133I IoT and XR](https://www.coursicle.com/ccsf/courses/CNIT/133I/) taught by [Professor Da Silva](https://cdasilva.info/) [CCSF](https://www.ccsf.edu/).  The project consist of both hardware and software.  The hardware consist of:
 
-## Available Scripts
+ESP32-CAM (AI Thinker)
+Arudino Uno
 
-In the project directory, you can run:
+Software consist of:
+
+Frontend web app made in React/Typescript
+[ESP32 Camera Web Server](https://github.com/easytarget/esp32-cam-webserver) by Easytarget
+
+## Project Folder Structure for React Web App
+```
+├── README.md # This file.
+├── .gitignore # Git untracked files.
+├── package.json # npm package manager configuration file for project
+├── src
+│   ├── App.js.jpg # Main app.
+
+└──
+    ├──
+    ├──
+    └──
+
+
+
+## Installation Instructions
 
 ### `npm start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Resources
 
-### `npm test`
+Github page for ESP32 Camera Webserver by Easytarget.  I like how he made an easy to use API. [https://github.com/easytarget/esp32-cam-webserver](https://github.com/easytarget/esp32-cam-webserver)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Good introduction on the ESP32 Cam on Youtube from Dronebot Workshop.  His Wifi Manager video and other videos are also quite good.  [https://www.youtube.com/watch?v=xPlN_Tk3VLQ&t=2090s](https://www.youtube.com/watch?v=xPlN_Tk3VLQ&t=2090s)
 
-### `npm run build`
+Pretty Good introduction to ESP32-Cam that shows off it’s face-recognition capability.  This one uses the web camera server code from Espressif, the company behind the ESP32, but the face recognition is limited to 320x240 and the stream is in MJEG, so I didn’t not to use it.[https://randomnerdtutorials.com/esp32-cam-video-streaming-face-recognition-arduino-ide/](https://randomnerdtutorials.com/esp32-cam-video-streaming-face-recognition-arduino-ide/)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+[Reveal.js](https://revealjs.com/).  My presentation was partially done using this HTML presentation framework.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,190 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Row, Col, Container, Form } from 'react-bootstrap'
+import RangeSlider from 'react-bootstrap-range-slider'
+import './custom.scss'
+// import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const CAMERA_URL = 'http://192.168.0.119'
+
+const MIN_BRIGHTNESS = -2.0
+const MAX_BRIGHTNESS = 2.0
+const MIN_SATURATION = -2.0
+const MAX_SATURATION = 2.0
+const MIN_CONTRAST = -2.0
+const MAX_CONTRAST = 2.0
+
+const SpecialEffects = [
+	"No Special Effect",
+	"Negative",
+	"Greyscale",
+	"Red Tint",
+	"Green Tint",
+	"Blue Tint",
+	"Sepia"
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [brightness, setBrightness] = useState(0)
+	const [saturation, setSaturation] = useState(0)
+	const [contrast, setContrast] = useState(0)
+	const [specialEffect, setSpecialEffect] = useState(0)
+
+	const handleBrightnessChange = (e) => {
+		const newBrightness = e.target.value
+		const oldBrightness = brightness
+
+		setBrightness(newBrightness)
+
+		fetch(`${CAMERA_URL}/control?var=brightness&val=${newBrightness}`)
+			.then(() => {
+				console.log('Brightness Change Success!')
+			})
+			.catch(err => {
+				console.log('Error changing brightness,', err)
+				setBrightness(oldBrightness)
+				toast.error('Problem with ESP32-CAM.  Brightness not changed.', {
+					autoClose: 3000
+				})
+			})
+	}
+
+	const handleSaturationChange = e => {
+		const newSaturdation = e.target.value
+		const oldSaturation = saturation
+
+		setSaturation(newSaturdation)
+
+		fetch(`${CAMERA_URL}/control?var=saturation&val=${newSaturdation}`)
+			.then(() => {
+				console.log('Saturation Change Success!')
+			})
+			.catch(err => {
+				console.log('Error changing saturation,', err)
+				setSaturation(oldSaturation)
+				toast.error('Problem with ESP32-CAM.  Saturation not changed', {
+					autoClose: 3000,
+				})
+			})
+	}
+
+	const handleContrastChange = e => {
+		const newContrast = e.target.value
+		const oldContrast = contrast
+
+		setContrast(newContrast)
+
+		fetch(`${CAMERA_URL}/control?var=contrast&val=${newContrast}`)
+			.then(() => {
+				setContrast(newContrast)
+				console.log('Contrast Change Success!')
+			})
+			.catch(err => {
+				console.log('Error changing contrast,', err)
+				setContrast(oldContrast)
+				toast.error('Contrast not changed', {
+					autoClose: 3000,
+				})
+			})
+	}
+
+	const handleSpecialEffectChange = e => {
+		const newSpecialEffect = e.target.value
+
+		fetch(
+			`${CAMERA_URL}/control?var=special_effect&val=${newSpecialEffect}`
+		)
+			.then(() => {
+				setSpecialEffect(newSpecialEffect)
+				console.log('Special Effect Change Success!')
+			})
+			.catch(err => {
+				console.log('Error changing special effect,', err)
+				toast.error('Special Effect not changed', {
+					autoClose: 3000,
+				})
+			})
+	}
+	return (
+		<div className='App'>
+			<ToastContainer theme='colored' />
+			<Container>
+				<Row>
+					<h1>Video from ESP32-Cam</h1>
+				</Row>
+				<Row>
+					<Col lg={8}>
+						Video
+						{/* <img
+							src={`${CAMERA_URL}:81/`}
+							width='640'
+							height='480'
+							alt=''
+						/> */}
+					</Col>
+					<Col lg={4}>
+						<Form className='p-1'>
+							<Form.Group className='p-1'>
+								<Form.Label>Brightness</Form.Label>
+								<RangeSlider
+									// className='m-3'
+									value={brightness}
+									step='0.1'
+									onChange={handleBrightnessChange}
+									// tooltipStyle="background: blue"
+									min={MIN_BRIGHTNESS}
+									max={MAX_BRIGHTNESS}
+									tooltipPlacement='bottom'
+									tooltip='auto'
+								/>
+								<Form.Label className='mt-5'>Saturation</Form.Label>
+								<RangeSlider
+									// className='mb-3'
+									value={saturation}
+									step='0.1'
+									onChange={handleSaturationChange}
+									min={MIN_SATURATION}
+									max={MAX_SATURATION}
+									tooltipPlacement='bottom'
+									tooltip='auto'
+								/>
+								<Form.Label className='mt-5'>Contrast</Form.Label>
+								<RangeSlider
+									// className='mb-3'
+									value={contrast}
+									step='0.1'
+									onChange={handleContrastChange}
+									min={MIN_CONTRAST}
+									max={MAX_CONTRAST}
+									tooltipPlacement='bottom'
+									tooltip='auto'
+								/>
+								<Form.Control
+									as='select'
+									value={specialEffect}
+									className='mt-5'
+									onChange={handleSpecialEffectChange}
+								>
+									<option>{SpecialEffects[specialEffect]}</option>
+									{SpecialEffects.map(
+										(item, index) =>
+											index !== +specialEffect && (
+												<option value={index} key={index}>
+													{item}
+												</option>
+											)
+									)}
+									)
+								</Form.Control>
+							</Form.Group>
+						</Form>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	)
 }
 
 export default App;
